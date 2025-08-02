a8320985e55b96bff95c6bc784fd8aa78f066d40
Marin Mema <marin.mema@homecarehub.com>
2025-08-02 11:26:40 -0400

    play ding.mp3 on tap

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { useState, useRef } from "react";
+import audiofile from "./assets/ding.mp3";
 import "./App.css";
 
 function App() {
+  const audio = useRef<HTMLAudioElement>(null);
   const [count, setCount] = useState(0);
+
+  function play() {
+    if (!audio.current) {
+      return;
+    }
+    audio.current.pause();
+    audio.current.currentTime = 0;
+    audio.current.play();
+  }
 
   function ding() {
     setCount(count + 1);
     console.log(count + 1);
+    play();
   }
 
   return (
     <div id="tap-surface" onClick={ding}>
-      <img src={reactLogo} className="logo react" alt="React logo" />
+      <audio src={audiofile} className="hidden" controls ref={audio}></audio>
+      <div className="tap-icon">🛎️</div>
     </div>
   );
 }
